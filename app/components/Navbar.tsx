99b54d8c811e0dc7d75c0cf9dc0bf6809329cae4
Nikola Stancic <88898574+Nikoraii@users.noreply.github.com>
2024-05-27 18:37:28 +0200

    Add TODO

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,6 +11,7 @@ export default function Navbar() {
                     <GiMoonBats className="w-10 h-10" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap text-gray-900">LUNERA</span>
                 </Link>
+                {/* TO DO: Mobile Menu implementaion */}
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
