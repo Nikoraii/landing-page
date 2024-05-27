04520e7372321316f56b3b124ee6c5bf7edaaae3
Nikola Stancic <88898574+Nikoraii@users.noreply.github.com>
2024-05-27 18:14:25 +0200

    Install react-icons and add icon to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import Link from "next/link";
+import { GiMoonBats } from "react-icons/gi";
 
 export default function Navbar() {
     return (
         <nav className="bg-white border-gray-200 fixed w-full border-b z-50">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <div>LOGO</div>
+                    <GiMoonBats className="w-10 h-10" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap text-gray-900">LUNERA</span>
                 </Link>
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
@@ -17,7 +18,7 @@ export default function Navbar() {
                     </svg>
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 text-gray-950 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
                         <li>
                         <a href="#home" className="nav-link" aria-current="page">Home</a>
                         </li>
